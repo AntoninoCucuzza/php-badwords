6d96a7fba64460e5ea0a4f13b47873283e6e9826
AntoninoCucuzza <cucuzza.antonino@hotmail.com>
2023-10-09 16:06:22 +0200

    add $censored  and html

--- script.php
+++ script.php
@@ -1,7 +1,10 @@
 <?php
 
-var_dump($_GET);
+//var_dump($_GET);
 
+$censored = '<span class="danger">⚠️ questa parola e illegale ⚠️</span>';
+
+$newParagraph =  str_replace($_GET['badword'], $censored,  $_GET['paragraph']);
 
 ?>
 
@@ -11,13 +14,43 @@ var_dump($_GET);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Script</title>
+    <title>Script php</title>
+    <style>
+        body {
+            color: white;
+            min-height: 100%;
+            background-color: black;
+        }
+
+        p {
+            font-size: 1.5rem;
+        }
+
+        .danger {
+            color: red;
+            padding: 0.25rem;
+            border: 3px solid red;
+            background-color: yellow;
+        }
+
+        .d-flex {
+            display: flex;
+            align-items: center;
+            flex-direction: column;
+        }
+    </style>
 </head>
 
 <body>
+    <div class="d-flex">
 
-    <h1>paragraph: <?php echo $_GET['paragraph'] ?></h1>
-    <h2>badword: <?php echo $_GET['badword'] ?></h2>
+        <h1>paragraph: <?php echo $_GET['paragraph'] ?></h1>
+
+        <h2>bad word: <?php echo $_GET['badword'] ?></h2>
+
+        <p><?php echo $newParagraph ?></p>
+
+    </div>
 
 </body>
 
